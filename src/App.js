535b784a4fba2ea10c6e9d7de708493d96d857b0
archiltavdgiridze <achitavdgiridze@gmail.com>
2023-05-09 00:45:21 +0400

    added sidebar and mainPage jsx code and styled the website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
 import Sidebar from "./Components/MainComponents/Sidebar";
@@ -7,7 +6,6 @@ import MainPage from "./Components/MainComponents/MainPage";
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
       <Sidebar />
       <MainPage />
     </div>
--- src/Components/MainComponents/Sidebar.jsx
+++ src/Components/MainComponents/Sidebar.jsx
@@ -1,43 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-            <div className="sidebar_title">
-              <a href="index.html">
-                <h1>ციტატელი</h1>
-              </a>
-            </div>
-            <div className="sidebar_content">
-              <ul>
-                <li>
-                  <a href="index.html">
-                    <i data-feather="home"></i>
-                    <p className="sidebar-texts">მთავარი</p>
-                  </a>
-                </li>
-                {/* <!-- <li>
-                  <a href="">
-                    <i data-feather="filter"></i>
-                    ფილტრი
-                  </a>
-                </li> --> */}
-                <li>
-                  <a href="about_us.html">
-                    <i data-feather="users"></i>
-                    <p className="sidebar-texts">ჩვენს შესახებ</p>
-                  </a>
-                </li>
-                {/* <!-- <li>
-                  <a href="contact.html">
-                    <i data-feather="phone"></i>
-                    კონტაქტი
-                  </a>
-                </li> --> */}
-              </ul>
-            </div>
-          </div>
-  )
-}
+      <div className="sidebar_title">
+        <a href="index.html">
+          <h1>ციტატელი</h1>
+        </a>
+      </div>
+      <div className="sidebar_content">
+        <ul>
+          <li>
+            <a href="index.html">
+              <i data-feather="home"></i>
+              <p className="sidebar-texts">მთავარი</p>
+            </a>
+          </li>
+          {/*<li>
+            <a href="">
+              <i data-feather="filter"></i>
+              ფილტრი
+            </a>
+          </li>*/}
+          <li>
+            <a href="about_us.html">
+              <i data-feather="users"></i>
+              <p className="sidebar-texts">ჩვენს შესახებ</p>
+            </a>
+          </li>
+          {/*<li>
+            <a href="contact.html">
+              <i data-feather="phone"></i>
+              კონტაქტი
+            </a>
+          </li>*/}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
