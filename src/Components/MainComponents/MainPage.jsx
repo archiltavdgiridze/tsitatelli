ec4ebecc1ecf25889ab72e078e267d011b5a0fe4
archiltavdgiridze <achitavdgiridze@gmail.com>
2023-05-09 12:59:01 +0400

    linked local database to react and it shows random quotes on website

--- src/Components/MainComponents/MainPage.jsx
+++ src/Components/MainComponents/MainPage.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import quoteBase from "../../quoteBase.json";
 
 const MainPage = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  
+  function genQuote() {
+    const randomIndex = Math.floor(Math.random() * quoteBase.length);
+    setQuote(quoteBase[randomIndex].quote);
+    setAuthor(quoteBase[randomIndex].nameSurname);
+  }
+
   return (
     <div className="result rightDiv">
       <div className="result_wrapper rightDivWrapper" id="data-container">
         <div className="generatorBtn" id="generatorBtn">
-          <button>ახლის გენერირება</button>
+          <button onClick={genQuote}>ახლის გენერირება</button>
         </div>
         <div className="quote">
           <i className="fa-solid fa-quote-left"></i>
-          <h3 className="generatedQuote">ციტატის ადგილი</h3>
+          <h3 className="generatedQuote">{quote}</h3>
         </div>
         <div className="author">
           <i className="fa-solid fa-pen-nib"></i>
-          <h3 className="generatedAuthor">ავტორის ადგილი</h3>
+          <h3 className="generatedAuthor">{author}</h3>
         </div>
       </div>
       <div className="dev-info">
--- src/Components/MainComponents/Sidebar.jsx
+++ src/Components/MainComponents/Sidebar.jsx
@@ -14,31 +14,14 @@ const Sidebar = () => {
       <div className="sidebar_content">
         <ul>
           <li>
-            {/* <a href="index.html"> */}
-              <i class="fa-solid fa-user"></i>
-              <FontAwesomeIcon icon={faHouse} />
-              <p className="sidebar-texts">მთავარი</p>
-            {/* </a> */}
+            <i className="fa-solid fa-user"></i>
+            <FontAwesomeIcon icon={faHouse} />
+            <p className="sidebar-texts">მთავარი</p>
           </li>
-          {/*<li>
-            <a href="">
-              <i data-feather="filter"></i>
-              ფილტრი
-            </a>
-          </li>*/}
           <li>
-            {/* <a href="about_us.html"> */}
-              <FontAwesomeIcon icon={faPerson} />
-
-              <p className="sidebar-texts">ჩვენს შესახებ</p>
-            {/* </a> */}
+            <FontAwesomeIcon icon={faPerson} />
+            <p className="sidebar-texts">ჩვენს შესახებ</p>
           </li>
-          {/*<li>
-            <a href="contact.html">
-              <i data-feather="phone"></i>
-              კონტაქტი
-            </a>
-          </li>*/}
         </ul>
       </div>
     </div>
