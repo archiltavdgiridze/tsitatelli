8ddf255b5ad0aec1ec9a3e19aac939476c6346b7
archiltavdgiridze <achitavdgiridze@gmail.com>
2023-05-09 13:55:22 +0400

    minor fixes 9.05 - 2

---
+++ src/Components/AboutUs/AboutUs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div className='result'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam eius obcaecati, praesentium sed animi nostrum aliquam quasi asperiores! Commodi, asperiores maiores molestiae alias aspernatur ad nam aut harum doloremque sunt?</div>
+  )
+}
+
+export default AboutUs
