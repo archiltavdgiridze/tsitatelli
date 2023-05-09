fa7db9f3101db1e7a7e7e8ad1849370190edc789
archiltavdgiridze <achitavdgiridze@gmail.com>
2023-05-09 16:05:03 +0400

    connected routes from sidebar to other pages, styled webpage for responsive, added icons and links to results side

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Sidebar from "./Components/MainComponents/Sidebar";
 import MainPage from "./Components/MainComponents/MainPage";
+import AboutUs from "./Components/AboutUs/AboutUs";
+// import Filter from "./Components/Filter/Filter";
+
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <MainPage />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        {/* <Route path="/filter" element={<Filter />} /> */}
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Filter/Filter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Filter = () => {
+  return (
+    <div>TESSSSSST 
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, aut voluptatum alias recusandae est neque sapiente illum eveniet velit optio aperiam iure suscipit eos facere quisquam pariatur repellendus nobis tempore ipsa? Porro assumenda asperiores inventore eaque fugit accusamus? Vitae minus ex temporibus architecto nobis reiciendis consectetur fuga corrupti repellat itaque?</div>
+  )
+}
+
+export default Filter
