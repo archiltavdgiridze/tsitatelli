246b65b81f981ab5e3d090c364e357ca23f0b681
archiltavdgiridze <achitavdgiridze@gmail.com>
2023-05-18 01:55:06 +0400

    changed icon on sidebar

--- src/Components/MainComponents/Sidebar.jsx
+++ src/Components/MainComponents/Sidebar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faPerson } from "@fortawesome/free-solid-svg-icons";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import Darkmode from "darkmode-js";
 
@@ -13,41 +14,41 @@ const Sidebar = () => {
     
     return (
       <div className="sidebar">
-      <div className="sidebar_title">
-        <Link to="/" className="sidebar_logo">
-          <h1>ციტატელი</h1>
-        </Link>
+        <div className="sidebar_title">
+          <Link to="/" className="sidebar_logo">
+            <h1>ციტატელი</h1>
+          </Link>
+        </div>
+        <div className="sidebar_content">
+          <ul>
+            <li>
+              <Link to="/" className="sidebar-links">
+                <span>
+                  <FontAwesomeIcon icon={faHouse} />
+                </span>
+                <p className="sidebar-texts">მთავარი</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/filter_by_authors" className="sidebar-links">
+                <span>
+                  <FontAwesomeIcon icon={faPerson} />
+                </span>
+                <p className="sidebar-texts">ავტორები</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/about_us" className="sidebar-links">
+                <span>
+                  <FontAwesomeIcon icon={faCircleInfo} />
+                </span>
+                <p className="sidebar-texts">ჩვენს შესახებ</p>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="sidebar_content">
-        <ul>
-          <li>
-            <Link to="/" className="sidebar-links">
-              <span>
-                <FontAwesomeIcon icon={faHouse} />
-              </span>
-              <p className="sidebar-texts">მთავარი</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/filter_by_authors" className="sidebar-links">
-              <span>
-                <FontAwesomeIcon icon={faPerson} />
-              </span>
-              <p className="sidebar-texts">ავტორები</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/about_us" className="sidebar-links">
-              <span>
-                <FontAwesomeIcon icon={faPerson} />
-              </span>
-              <p className="sidebar-texts">ჩვენს შესახებ</p>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Sidebar;
