eb7bd16e822704fae919d748bad5fe938eb98aa8
archiltavdgiridze <achitavdgiridze@gmail.com>
2023-05-20 22:16:13 +0400

    added filtered component details and styled them

--- src/Components/FilterByAuthors/Filtered.jsx
+++ src/Components/FilterByAuthors/Filtered.jsx
@@ -1,29 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const Filtered = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
+  const [authorName, setAuthorName] = useState("");
+
   useEffect(() => {
     if (!state?.filteredQuotes) {
       navigate("/filter_by_authors");
+    } else {
+      setAuthorName(state.filteredQuotes[0]?.nameSurname || "");
     }
   }, [state]);
+
+  const handleGoBack = () => {
+    navigate("/filter_by_authors");
+  };
+
   return (
-    <>
-      <div className="result FBA_result">
-        <div className="FBA_quotes">
-          {state?.filteredQuotes &&
-            state.filteredQuotes.map((quote) => (
-              <div key={quote.id}>
-                <h2>{quote.quote}</h2>
-                {/* <p>{quote.source}</p> */}
-                {/* <p>{quote.topic}</p> */}
-              </div>
-            ))}
-        </div>
+    <div className="result FBA_filtered">
+      <button className="backButton" onClick={handleGoBack}>
+        <FontAwesomeIcon icon={faArrowLeft} />
+      </button>
+      {authorName && <h1 className="filtered_authorName">{authorName}</h1>}
+
+      <div className="FBA_filtered_quotes">
+        {state?.filteredQuotes &&
+          state.filteredQuotes.map((quote) => (
+            <div key={quote.id}>
+              <h2>{quote.quote}</h2>
+              {/* <p>{quote.source}</p>
+              <p>{quote.topic}</p> */}
+            </div>
+          ))}
       </div>
-    </>
+    </div>
   );
 };
 
