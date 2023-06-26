5a703de7c6582ddc5706a01791a52fb2402925ec
archiltavdgiridze <achitavdgiridze@gmail.com>
2023-06-26 19:40:20 +0400

    added differentiated search bar for author and source elements

--- src/Components/Filter/MiniFilter/AuthorFilt.jsx
+++ src/Components/Filter/MiniFilter/AuthorFilt.jsx
@@ -59,17 +59,11 @@ const AuthorFilt = () => {
   return (
     <div className="author_filter">
       <div className="filter_container">
-        {/* <input 
-          className="search_bar"
-          type="text"
+        <Search
           value={searchQuery}
           onChange={handleSearchChange}
-          placeholder="მოძებნე ავტორი..."
-        /> */}
-        <Search 
-          value={searchQuery}
-          onChange={handleSearchChange}
-          placeholder="მოძებნე ავტორი..."/>
+          placeholder="ძიება ავტორის მიხედვით..."
+        />
 
         {sortedFirstLetters.map((letter) => {
           const authorsWithLetter = filteredAuthors.filter(
@@ -100,112 +94,3 @@ const AuthorFilt = () => {
 };
 
 export default AuthorFilt;
-
-// import React, { useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-
-// const AuthorFilt = () => {
-//   const url =
-//     "https://dev-george1meshveliani-api.pantheonsite.io/meshveliani/apis/georgian-quotes";
-
-//   const [authors, setAuthors] = useState([]);
-//   const [searchQuery, setSearchQuery] = useState("");
-//   const [filteredAuthors, setFilteredAuthors] = useState([]);
-
-//   const navigate = useNavigate();
-
-//   // async function logJSONData() {
-//   //   let url =
-//   //     "https://dev-george1meshveliani-api.pantheonsite.io/meshveliani/apis/georgian-quotes";
-//   //   const response = await fetch(url);
-//   //   const jsonData = await response.json();
-//   // console log all of the topics array together
-//   // console.log(jsonData.data.map((topic) => topic.attributes.topic));
-//   // }
-
-//   // logJSONData();
-
-//   const filterAuthors = (query) => {
-//     const filtered = authors.filter((author) =>
-//       author.toLowerCase().includes(query.toLowerCase())
-//     );
-//     setFilteredAuthors(filtered);
-//   };
-
-//   useEffect(() => {
-//     fetch(url)
-//       .then((response) => response.json())
-//       .then((db) => {
-//         const authorNames = db.data.map((author) => author.attributes.author);
-
-//         const georgianCollator = new Intl.Collator("ka-GE", {
-//           sensitivity: "base",
-//           ignorePunctuation: true,
-//         });
-
-//         authorNames.sort((a, b) => georgianCollator.compare(a, b));
-
-//         const uniqueAuthorNames = Array.from(new Set(authorNames));
-//         setAuthors(uniqueAuthorNames);
-//         filterAuthors(searchQuery);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching author data:", error);
-//       });
-//   }, [searchQuery]);
-
-//   const handleSearchChange = (event) => {
-//     const query = event.target.value;
-//     setSearchQuery(query);
-//   };
-
-//   const handleAuthorClick = (authorName) => {
-//     navigate(
-//       `/author-results/${encodeURIComponent(
-//         authorName.replace(/\s+|-|–/g, "-")
-//       )}`
-//     );
-//   };
-
-//   const sortedFirstLetters = [
-//     ...new Set(authors.map((author) => author.charAt(0))),
-//   ].sort();
-
-//   return (
-//     <div className="author_filter">
-//       <div className="filter_container">
-//         <input
-//           type="text"
-//           value={searchQuery}
-//           onChange={handleSearchChange}
-//           placeholder="Search authors..."
-//         />
-//         {sortedFirstLetters.map((letter) => {
-//           const authorsWithLetter = authors.filter(
-//             (author) => author.charAt(0) === letter
-//           );
-//           if (authorsWithLetter.length > 0) {
-//             return (
-//               <div key={letter} className="filt_cont_div">
-//                 <h2>{letter}</h2>
-//                 <div className="filt_cont_btn">
-//                   {authorsWithLetter.map((author) => (
-//                     <button
-//                       key={author}
-//                       onClick={() => handleAuthorClick(author)}
-//                     >
-//                       {author}
-//                     </button>
-//                   ))}
-//                 </div>
-//               </div>
-//             );
-//           }
-//           return null;
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AuthorFilt;
--- src/Components/Filter/MiniFilter/SourceFilt.jsx
+++ src/Components/Filter/MiniFilter/SourceFilt.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Search from "../../ReComp/Search";
 
 const SourceFilt = () => {
   const url =
     "https://dev-george1meshveliani-api.pantheonsite.io/meshveliani/apis/georgian-quotes";
 
   const [sources, setSources] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredSources, setFilteredSources] = useState([]);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -23,6 +27,7 @@ const SourceFilt = () => {
 
         const uniqueSourceNames = Array.from(new Set(sourceNames));
         setSources(uniqueSourceNames);
+        setFilteredSources(uniqueSourceNames);
       })
       .catch((error) => {
         console.error("Error fetching source data:", error);
@@ -37,6 +42,16 @@ const SourceFilt = () => {
     );
   };
 
+  const handleSearchChange = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+
+    const filtered = sources.filter((source) =>
+      source.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredSources(filtered);
+  };
+
   const sortedFirstLetters = [
     ...new Set(sources.map((source) => source.charAt(0))),
   ].sort();
@@ -44,8 +59,14 @@ const SourceFilt = () => {
   return (
     <div className="source_filter">
       <div className="filter_container">
+        <Search
+          value={searchQuery}
+          onChange={handleSearchChange}
+          placeholder="ძიება წყაროს მიხედვით..."
+        />
+
         {sortedFirstLetters.map((letter) => {
-          const sourcesWithLetter = sources.filter(
+          const sourcesWithLetter = filteredSources.filter(
             (source) => source.charAt(0) === letter
           );
           if (sourcesWithLetter.length > 0) {
