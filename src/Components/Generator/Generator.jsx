b9b1243441dd3fa0aeb50de9c27764c8c5c7cec5
archiltavdgiridze <achitavdgiridze@gmail.com>
2023-07-12 12:29:30 +0400

    fixed generator component bugs

--- src/Components/Generator/Generator.jsx
+++ src/Components/Generator/Generator.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./generator.css";
 import CopyButton from "../ReComp/CopyButton";
 import MultipleSelectChip from "../ReComp/ChipSelector";
 import { API_ENDPOINT } from "../../quoteURL";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
-import { useLocation, useNavigate } from "react-router-dom";
 
 const Generator = () => {
   const url = API_ENDPOINT;
   const navigate = useNavigate();
-
   const [authorNames, setAuthorNames] = useState([]);
   const [selectedAuthors, setSelectedAuthors] = useState([]);
   const [topicNames, setTopicNames] = useState([]);
@@ -18,7 +17,7 @@ const Generator = () => {
   const [sourceNames, setSourceNames] = useState([]);
   const [selectedSources, setSelectedSources] = useState([]);
   const [generatedQuotes, setGeneratedQuotes] = useState([]);
-  const [activeCardIndex, setActiveCardIndex] = useState(null);
+  const [activeCardIndices, setActiveCardIndices] = useState([]);
 
   useEffect(() => {
     fetch(url)
@@ -117,23 +116,33 @@ const Generator = () => {
     const groupedQuotes = {};
 
     selectedAuthors.forEach((author) => {
-      const quotesByAuthor = quotes.filter((quote) => quote.author === author);
-      groupedQuotes[author] = groupedQuotes[author] || [];
-      groupedQuotes[author].push(...quotesByAuthor);
+      const uniqueQuotes = new Set(); // Use a Set to store unique quotes
+      quotes.forEach((quote) => {
+        if (quote.author === author) {
+          uniqueQuotes.add(quote); // Add quotes matching the selected author to the set
+        }
+      });
+      groupedQuotes[author] = Array.from(uniqueQuotes); // Convert the set to an array and assign it to the corresponding author key
     });
 
     selectedTopics.forEach((topic) => {
-      const quotesByTopic = quotes.filter((quote) =>
-        quote.topic.includes(topic)
-      );
-      groupedQuotes[topic] = groupedQuotes[topic] || [];
-      groupedQuotes[topic].push(...quotesByTopic);
+      const uniqueQuotes = new Set(); // Use a Set to store unique quotes
+      quotes.forEach((quote) => {
+        if (quote.topic.includes(topic)) {
+          uniqueQuotes.add(quote); // Add quotes matching the selected topic to the set
+        }
+      });
+      groupedQuotes[topic] = Array.from(uniqueQuotes); // Convert the set to an array and assign it to the corresponding topic key
     });
 
     selectedSources.forEach((source) => {
-      const quotesBySource = quotes.filter((quote) => quote.source === source);
-      groupedQuotes[source] = groupedQuotes[source] || [];
-      groupedQuotes[source].push(...quotesBySource);
+      const uniqueQuotes = new Set(); // Use a Set to store unique quotes
+      quotes.forEach((quote) => {
+        if (quote.source === source) {
+          uniqueQuotes.add(quote); // Add quotes matching the selected source to the set
+        }
+      });
+      groupedQuotes[source] = Array.from(uniqueQuotes); // Convert the set to an array and assign it to the corresponding source key
     });
 
     return groupedQuotes;
@@ -163,12 +172,28 @@ const Generator = () => {
     navigate(`/source-results/${decodedSourceName}`);
   };
 
-  const handleCardClick = (index) => {
-    if (activeCardIndex === index) {
-      setActiveCardIndex(null);
-    } else {
-      setActiveCardIndex(index);
-    }
+  const handleCardClick = (groupIndex, cardIndex) => {
+    setActiveCardIndices((prevIndices) => {
+      //creating a copy of the previous activeCardIndices state using the spread operator
+      const updatedIndices = [...prevIndices];
+
+      if (updatedIndices[groupIndex] === cardIndex) {
+        // Clicked on the already active card, so remove the active state
+        updatedIndices[groupIndex] = null;
+      } else {
+        // Clicked on a different card, update the active state
+        updatedIndices[groupIndex] = cardIndex;
+
+        // Remove the active state from the previously active card
+        for (let i = 0; i < updatedIndices.length; i++) {
+          if (i !== groupIndex) {
+            updatedIndices[i] = null;
+          }
+        }
+      }
+
+      return updatedIndices;
+    });
   };
 
   return (
@@ -208,70 +233,84 @@ const Generator = () => {
         </div>
         <hr />
         <div className="card_groups">
-          {Object.entries(generatedQuotes).map(([category, quotes]) => (
-            <div className="card_group">
-              <h2 className="gen_category_title">{category}</h2>
-              <div key={category} className="cards gen_cards">
-                {quotes.map((data, index) => (
-                  <div key={index} className="quote_card gen_quote_card">
+          {Object.entries(generatedQuotes).map(
+            ([category, quotes], groupIndex) => (
+              <div className="card_group" key={category}>
+                <h2 className="gen_category_title">{category}</h2>
+                <div className="cards gen_cards">
+                  {quotes.map((data, cardIndex) => (
                     <div
-                      className={`info_btn ${
-                        activeCardIndex === index ? "active" : ""
-                      }`}
-                      onClick={() => handleCardClick(index)}
+                      key={`${category}_${cardIndex}`}
+                      className="quote_card gen_quote_card"
                     >
-                      <FontAwesomeIcon
-                        className="quote_info"
-                        icon={faCircleInfo}
-                      />
-                    </div>
-                    <figcaption className="q_card_top">
-                      <h2>„{data.quote}“</h2>
-                    </figcaption>
-                    <figcaption
-                      className={`q_card_bottom ${
-                        activeCardIndex === index ? "active" : ""
-                      }`}
-                      style={{
-                        display: activeCardIndex === index ? "flex" : "none",
-                      }}
-                    >
-                      <div className="q_card_buttons">
-                        <div className="top_group_buttons">
-                          <p className="group_title">წყარო:</p>
-                          <button
-                            className="linker_source linkers"
-                            onClick={() => handleSourceClick(data.source)}
-                          >
-                            <p>{data.source}</p>
-                          </button>
-                        </div>
-                        <div className="bottom_group_buttons">
-                          <p className="group_title">თემატიკა:</p>
-                          {splitTopics(data.topic).map((topic, topicIndex) => (
-                            <button
-                              key={`${data.id}_${topicIndex}`}
-                              className="linker_topic linkers"
-                              onClick={() => handleTopicClick(topic)}
-                            >
-                              <p>{topic}</p>
-                            </button>
-                          ))}
-                        </div>
-                      </div>
-                      <div className="card_copy">
-                        <CopyButton
-                          text={`„${data.quote}“ 
-- ${data.author}`}
-                          className="copy-btn btn_filled"
+                      <div
+                        className={`info_btn ${
+                          activeCardIndices[groupIndex] === cardIndex
+                            ? "active"
+                            : ""
+                        }`}
+                        onClick={() => handleCardClick(groupIndex, cardIndex)}
+                      >
+                        <FontAwesomeIcon
+                          className="quote_info"
+                          icon={faCircleInfo}
                         />
                       </div>
-                    </figcaption>
-                  </div>
-                ))}
+                      <figcaption className="q_card_top">
+                        <h2>„{data.quote}“</h2>
+                      </figcaption>
+                      <figcaption
+                        className={`q_card_bottom ${
+                          activeCardIndices[groupIndex] === cardIndex
+                            ? "active"
+                            : ""
+                        }`}
+                        style={{
+                          display:
+                            activeCardIndices[groupIndex] === cardIndex
+                              ? "flex"
+                              : "none",
+                        }}
+                      >
+                        <div className="q_card_buttons">
+                          <div className="top_group_buttons">
+                            <p className="group_title">წყარო:</p>
+                            <button
+                              className="linker_source linkers"
+                              onClick={() => handleSourceClick(data.source)}
+                            >
+                              <p>{data.source}</p>
+                            </button>
+                          </div>
+                          <div className="bottom_group_buttons">
+                            <p className="group_title">თემატიკა:</p>
+                            {splitTopics(data.topic).map(
+                              (topic, topicIndex) => (
+                                <button
+                                  key={`${data.id}_${topicIndex}`}
+                                  className="linker_topic linkers"
+                                  onClick={() => handleTopicClick(topic)}
+                                >
+                                  <p>{topic}</p>
+                                </button>
+                              )
+                            )}
+                          </div>
+                        </div>
+                        <div className="card_copy">
+                          <CopyButton
+                            text={`„${data.quote}“ 
+- ${data.author}`}
+                            className="copy-btn btn_filled"
+                          />
+                        </div>
+                      </figcaption>
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
       </div>
     </div>
